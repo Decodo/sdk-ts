5b7d81b5f9d2b63641df06a15eb040d3e40e9a85
domantas-jurkus-dcd <domantas.jurkus@decodo.com>
2025-08-25 14:27:26 +0300

    feat: add Scraper API endpoint consts (#8)

---
+++ src/scraper-api/constants.ts
@@ -0,0 +1,7 @@
+export const SCRAPER_API_URL = "https://scraper-api.decodo.com";
+
+export const SCRAPER_API_ENDPOINT_SYNC = `${SCRAPER_API_URL}/v2/scrape`;
+
+export const SCRAPER_API_ENDPOINT_ASYNC = `${SCRAPER_API_URL}/v2/task`;
+
+export const SCRAPER_API_ENDPOINT_BATCH = `${SCRAPER_API_URL}/v2/task/batch`;
